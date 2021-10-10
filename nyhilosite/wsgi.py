efc01c915488889e8fe41f24b01fce29746771fb
Nyhilo <jacclowa@gmail.com>
2021-10-10 06:04:42 +0000

    Add settings.py to .gitignore again.

--- nyhilosite/wsgi.py
+++ nyhilosite/wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
-sys.path.append(os.getenv('WSGI_SYS_PATH'))
-os.environ.setdefault('PYTHON_EGG_CACHE', os.getenv('PYTHON_EGG_CACHE'))
+sys.path.append('/opt/bitnami/apps/django/django_projects/nyhilosite')
+os.environ.setdefault('PYTHON_EGG_CACHE', '/opt/bitnami/apps/django/django_projects/nyhilosite/egg_cache')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nyhilosite.settings')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
