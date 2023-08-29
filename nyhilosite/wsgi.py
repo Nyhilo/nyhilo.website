856d8525e5d1c634f250c3d44262c23ec0674ad7
Nyhilo <jacclowa@gmail.com>
2023-08-28 23:14:47 -0700

    Clean slate for Django 4 redo.

--- nyhilosite/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-sys.path.append('/opt/bitnami/apps/django/django_projects/nyhilosite')
-os.environ.setdefault('PYTHON_EGG_CACHE', '/opt/bitnami/apps/django/django_projects/nyhilosite/egg_cache')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nyhilosite.settings')
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
