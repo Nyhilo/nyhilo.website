3bf94c23c924b505c15c02dc0adafddf998032f9
Nyhilo <jacclowa@gmail.com>
2023-08-30 18:36:35 -0700

    Put powers into database then load them in the template

--- stupidpowers/models.py
+++ stupidpowers/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class StupidPower(models.Model):
     power = models.CharField(max_length=200)
-    ruling = models.CharField(max_length=2048)
+    ruling = models.CharField(max_length=2048, default='')
--- stupidpowers/views.py
+++ stupidpowers/views.py
@@ -7,20 +7,5 @@ from .models import StupidPower
 
 
 def index(request):
-    return render(request, 'stupidpowers/index.html')
-
-
-def powers(request, desc: str = None):
-    if request.method == 'GET':
-        json = [{'power': s.power, 'ruling': s.ruling} for s in StupidPower.objects.all()]
-
-        return JsonResponse(json, safe=False)
-
-    elif request.method == 'POST':
-        if desc is None:
-            return HttpResponseBadRequest(
-                'Please include a "desc" query param when making a post request to this route')
-
-        StupidPower.objects.create(power=desc)
-
-    return HttpResponseBadRequest('The server does not support that method.')
+    powers = [{'power': s.power, 'ruling': s.ruling} for s in StupidPower.objects.all()]
+    return render(request, 'stupidpowers/index.html', {'powers': powers})
