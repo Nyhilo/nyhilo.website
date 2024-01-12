74045e10eedc5b62f51d5b6e9f58382c2eb9b108
Nyhilo <jacclowa@gmail.com>
2024-01-11 18:08:34 -0700

    Store map background in database as well

--- cycle15/models.py
+++ cycle15/models.py
@@ -6,3 +6,15 @@ class Sprite(models.Model):
     x = models.IntegerField(default=0)
     y = models.IntegerField(default=0)
     image = models.CharField(max_length=2**16)  # 64kb
+    active = models.BooleanField(default=True)
+
+
+class MapBackground(models.Model):
+    imageData = models.CharField(max_length=2**32)   # 4gb
+    created = models.DateTimeField()
+
+
+class GamestateMap(models.Model):
+    imageData = models.CharField(max_length=2**32)
+    created = models.DateTimeField()
+    filename = models.CharField(max_length=64)
--- cycle15/views.py
+++ cycle15/views.py
@@ -1,6 +1,6 @@
 import base64
 import json
-import os
+from datetime import datetime
 
 
 from django.shortcuts import render
@@ -9,7 +9,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django import forms
 
 
-from .models import Sprite
+from .models import Sprite, MapBackground
 from .c15image import overlay_sprites
 from nyhilosite.settings import env
 from nyhilosite.settings import C15_BASE_IMAGE, C15_GAMESTATE_IMAGE
@@ -18,24 +18,26 @@ from nyhilosite.settings import C15_BASE_IMAGE, C15_GAMESTATE_IMAGE
 # VIEWS #
 @ensure_csrf_cookie
 def index(request):
-    # return HttpResponse('hewwow')
-    return render(request, 'cycle15/index.html', {'sprites': Sprite.objects.all()})
+    sprites = Sprite.objects.all()
+    background = MapBackground.objects.last()
+    return render(request, 'cycle15/index.html', {'background': background, 'sprites': sprites})
 
 
-def upload_sprite_image(request):
+def upload(request):
     if request.method == 'GET':
-        return render(request, 'cycle15/spriteupload.html')
+        return render(request, 'cycle15/upload.html')
 
+
+# POST
+def upload_sprite_image(request):
     if request.method == 'POST':
         form = SpriteForm(request.POST, request.FILES)
         if form.is_valid():
             username = form.cleaned_data['Username']
-            x_position = form.cleaned_data['x_position']
-            y_position = form.cleaned_data['y_position']
             file_upload = form.cleaned_data['file_upload']
 
             if Sprite.objects.filter(user=username).exists():
-                return render(request, 'cycle15/spriteupload.html')
+                return render(request, 'cycle15/upload.html')
 
             # Convert the uploaded file to base64
             encoded_file = None
@@ -50,17 +52,41 @@ def upload_sprite_image(request):
 
                 file_string = f'data:image/{extension};base64,{encoded_file}'
 
-            Sprite.objects.create(
-                user=username, x=x_position, y=y_position, image=file_string)
+            Sprite.objects.create(user=username, x=0, y=0, image=file_string)
 
-            # Redirect or render a success page
-            return render(request, 'cycle15/spriteupload.html')
-    else:
-        form = SpriteForm()
-    return render(request, 'cycle15/spriteupload.html', {})
+            # Redirect back to upload page
+            return render(request, 'cycle15/upload.html')
+
+    return render(request, 'cycle15/upload.html', {})
 
 
-# POST
+def upload_background_image(request):
+    if request.method == 'POST':
+        form = BackgroundForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_upload = form.cleaned_data['file_upload']
+
+            # Convert the uploaded file to base64
+            encoded_file = None
+            if file_upload:
+                encoded_file = base64.b64encode(
+                    file_upload.read()).decode('utf-8')
+
+                extension = file_upload._name.split('.')[-1]
+
+                if extension not in ['png', 'jpg', 'jpeg', 'webp']:
+                    return HttpResponseBadRequest('Bad file extension. Use png, jpg, or webp')
+
+                file_string = f'data:image/{extension};base64,{encoded_file}'
+
+            MapBackground.objects.create(imageData=file_string, created=datetime.utcnow())
+
+            # Redirect back to upload page
+            return render(request, 'cycle15/upload.html')
+
+    return render(request, 'cycle15/upload.html', {})
+
+
 def saveSprites(request):
     # Validate the attributes
     data = json.loads(request.body)
@@ -97,6 +123,8 @@ def get_current_map(request):
 
 class SpriteForm(forms.Form):
     Username = forms.CharField(max_length=100)
-    x_position = forms.IntegerField()
-    y_position = forms.IntegerField()
+    file_upload = forms.FileField()
+
+
+class BackgroundForm(forms.Form):
     file_upload = forms.FileField()
